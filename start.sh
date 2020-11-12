69cd53de45b530f45131ba8914b23e8c61490a44
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-12 17:39:47 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 python3 -m pip install setuptools
 python3 -m pip install requests
+git config --global user.email "no-reply@kamakepar.ru"
+git config --global user.name "Server Transformer"
+git add *
+git commit -m "Upload"
+git push
 python3 main.py
