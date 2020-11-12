b5371fc63182eadf7be7aa56aa10c605e741cc3a
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-12 17:32:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+from datetime import datetime
+import requests as r
+
+lol = r.get('https://raw.githubusercontent.com/Kamakepar2029/allnerwork/main/res.txt')
+
+f = open("res.txt", "a")
+f.write(lol.text)
+f.close()
+
+res = open("res.txt","r").read()
+
+example = 'Host: 175.119.133.197 ()	Status: Up'
+
+mass = res.split('\n')
+
+start = 0
+end = len(mass)
+
+html = ''
+
+while start < end:
+  pol = mass[start].split(' ')
+  print(pol)
+  if pol[0] == 'Host:':
+    if pol[2] == '()\tStatus:':
+      if pol[3] == 'Up':
+        print(Fore.GREEN+'New Host: '+pol[1]+' Status: '+pol[3])
+        html = html + '<p class="all"><a class="status">Status: '+pol[3]+'</a><a class="href" href="http://'+pol[1]+'">'+pol[1]+'</a></p>'
+      else:
+        print(Fore.RED + 'No hot up')
+    else:
+      print(Fore.RED + 'No new Host up')
+  else:
+    print(Fore.RED + 'No new Host down or up')
+  start+=1
+
+os.system('mkdir '+datetime.today().strftime('%d.%m.%Y'))
+ls = datetime.today().strftime('%d.%m.%Y')
+f = open(ls+"/index.html", "a")
+f.write(html)
+f.close()
+
+print(html)
